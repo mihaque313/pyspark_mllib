4e949356eb21745004198eb05d4eaccbbb5b578f
Md Inzamamul Haque <RLE0594@rlpl.com>
2018-10-06 00:46:40 +0530

    some minor changes

--- Pyspark_Sample_ML_programs/binarizer.py
+++ Pyspark_Sample_ML_programs/binarizer.py
@@ -2,7 +2,7 @@ from pyspark.ml.feature import Binarizer
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 
-sc = SparkContext("local", "samp")
+sc = SparkContext("local","samp")
 sqlContext = SQLContext(sc)
 continuousDF = sqlContext.createDataFrame([(0, 0.1), (1, 0.8), (2, 0.8)], ['label', 'features'])
 binarizer = Binarizer(inputCol="features", outputCol="binarized_feature", threshold=0.5)
