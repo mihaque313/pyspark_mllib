8342108384d15b7f2169aff190038e176d7533a9
Md Inzamamul Haque <RLE0594@rlpl.com>
2018-10-06 00:50:51 +0530

    some minor changes

--- Pyspark_Sample_ML_programs/chiSqSelector.py
+++ Pyspark_Sample_ML_programs/chiSqSelector.py
@@ -8,5 +8,5 @@ sqlContext = SQLContext(sc)
 data = sqlContext.createDataFrame([(7, Vectors.dense([0.0, 0.0, 18.0, 1.0]), 1.0),
                                    (8, Vectors.dense([0.0, 1.0, 12.0, 0.0]), 0.0),
                                    (9, Vectors.dense([1.0, 0.0, 15.0, 0.1]), 0.0)
-                                   ], ["id", "features", "clicked"])
+                                  ], ["id", "features", "clicked"])
 selector=Statistics.chiSqTest(data)#ChiSqSelector is not available in Statistics instead chiSqTest
