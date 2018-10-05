1e182f495116bee064f94e5f9267310f43703ea8
Md Inzamamul Haque <RLE0594@rlpl.com>
2018-10-06 00:44:12 +0530

    some minor changes

--- Pyspark_Sample_ML_programs/SQLTransformer.py
+++ Pyspark_Sample_ML_programs/SQLTransformer.py
@@ -2,7 +2,7 @@ from pyspark.ml.feature import SQLTransformer
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 
-sc = SparkContext("local", "samp")
+sc = SparkContext("local","samp")
 sqlContext = SQLContext(sc)
 df=sqlContext.createDataFrame([(0,1.0,2.0),(1,3.0,4.0),(2,5.0,6.0)],["id","v1","v2"])
 sqlTrans=SQLTransformer(statement="SELECT *,(v1+v2) as v3,(v1*v2) as v4 from __THIS__")
